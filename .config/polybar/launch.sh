46490f7df7f7a3e76131b9053c78666c27e99a3e
Reyyancli <reyyan19808@gmail.com>
2025-12-09 19:29:58 +0600

    i3 reconfiguration and better polybar implementation with i3 module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,12 @@
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar | tee -a /tmp/polybar1.log & disown
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 echo "Bars launched.."
